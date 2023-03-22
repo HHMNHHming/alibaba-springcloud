26b3c52e62f7dab73004500d8837243c49215532
GW00307172 <gw00307172@tech.gwm.cn>
2023-03-22 17:53:22 +0800

    初始化阿里nacos服务

---
+++ alibaba-nacos/src/main/java/com/gwm/NacosApplication.java
@@ -0,0 +1,12 @@
+package com.gwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class);
+    }
+}
---
+++ alibaba-nacos/src/test/java/com/gwm/MyTest.java
@@ -0,0 +1,13 @@
+package com.gwm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+public class MyTest {
+
+    @Test
+    public void test(){
+        System.out.println("测试方法正在执行···");
+    }
+}
