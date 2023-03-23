e8be5628daa5386a479999fad2a4baca62aa0d54
GW00307172 <gw00307172@tech.gwm.cn>
2023-03-23 10:05:09 +0800

    导入客户端依赖

--- alibaba-nacos/src/main/java/com/gwm/NacosApplication.java
+++ alibaba-nacos/src/main/java/com/gwm/NacosApplication.java
@@ -2,8 +2,9 @@ package com.gwm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NacosApplication {
     public static void main(String[] args) {
